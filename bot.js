89bd9b591d3da690d9b71d941253b695ccd8fe80
Igibek Koishybayev <koishybayev.igibek@gmail.com>
2017-11-28 18:41:37 -0500

    update

--- bot.js
+++ bot.js
@@ -105,7 +105,7 @@ app.post("/travis",function(req,res){
 
 // connect the bot to a stream of messages
 var bot = controller.spawn({
-  token: "xoxb-252577468864-EvT7Jzc307J0JqZCZHOGcFoC",
+  token: process.env.SLACK_TOKEN,
 }).startRTM()
 
 //add token
